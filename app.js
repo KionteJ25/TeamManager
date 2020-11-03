d97cff7a37fc83969af7d9e67769f3e4f66cdd03
Kionte Jones <jkionte20@gmail.com>
2020-11-03 18:02:03 -0500

    npm installed, added more prompts for each class and build team function

--- app.js
+++ app.js
@@ -17,7 +17,57 @@ const arrayID = [];
 // and to create objects for each team member (using the correct classes as blueprints!)
 function appMenu() {
     function createManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+                        return "Please enter a valid name"
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's id?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+                        return "Please enter a valid id"
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+                        return "Please enter a vaild email"
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number?",
+                validate: answer => {
+                    if(answer !== "") {
+                        return true
+                    }
+                        return "Please enter a valid office number"
+                }
+            }
 
+        ]).then(answers => {
+            const engineer = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+            teamMembers.push(manager)
+            arrayId.push(answers.managerId)
+        });
     }
 
     function createEngineer() {
@@ -25,7 +75,7 @@ function appMenu() {
             {
                 type: "input",
                 name: "engineerName",
-                message: "What is your engineer's name",
+                message: "What is your engineer's name?",
                 validate: answer => {
                     if(answer !== "") {
                         return true
@@ -37,7 +87,7 @@ function appMenu() {
             {
                 type: "input",
                 name: "engineerId",
-                message: "What is your employee's ID?",
+                message: "What is your engineers's ID?",
                 validate: answer => {
                     if(answer !== "") {
                         return true
@@ -49,7 +99,7 @@ function appMenu() {
             {
                 type: "input",
                 name: "engineerEmail",
-                message: "What is your employee's email?",
+                message: "What is your engineer's email?",
                 validate: answer => {
                     if(answer !== "") {
                         return true
@@ -61,7 +111,7 @@ function appMenu() {
             {
                 type: "input",
                 name: "engineerGithub",
-                message: "What is your employee's Github?",
+                message: "What is your engineer's Github?",
                 validate: answer => {
                     if(answer !== "") {
                         return true
@@ -73,9 +123,65 @@ function appMenu() {
         ]).then(answers => {
             const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
             teamMembers.push(engineer)
-            arrayId.push(answers.engineerId)
+            arrayId.push(answers.engineerId)           
+        });
+    }
+            function createIntern() {
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "internName",
+                        message: "What is your intern's name?",
+                        validate: answer => {
+                            if(answer !== "") {
+                                return true
+                            }
+        
+                            return "Please enter a valid name"
+                        }
+                    },
+                    {
+                        type: "input",
+                        name: "internId",
+                        message: "What is your intern's ID?",
+                        validate: answer => {
+                            if(answer !== "") {
+                                return true
+                            }
+        
+                            return "Please enter a valid ID"
+                        }
+                    },
+                    {
+                        type: "input",
+                        name: "internEmail",
+                        message: "What is your intern's email?",
+                        validate: answer => {
+                            if(answer !== "") {
+                                return true
+                            }
+        
+                            return "Please enter a valid email"
+                        }
+                    },
+                    {
+                        type: "input",
+                        name: "internSchool",
+                        message: "What is your intern's school?",
+                        validate: answer => {
+                            if(answer !== "") {
+                                return true
+                            }
+        
+                            return "Please enter a valid school"
+                        }
+                    }
+        ]).then(answers => {
+            const engineer = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+            teamMembers.push(intern)
+            arrayId.push(answers.internId)
             // run a function here that creates the entire "team" prompting you to create another employee
-        })
+        });
     }
 
     function buildTeam() {
@@ -85,22 +191,3 @@ function appMenu() {
         fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
     }
 };
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
